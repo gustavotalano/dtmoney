822ba10cdc90d1129649b3b73db1435ac087cfe2
Gustavo Talano <gustavotalano@hotmail.com>
2021-10-15 13:54:55 -0300

    feat: adds new features into app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header"
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from "./styles/global"
+import { TransactionsProvider } from './TransactionContext';
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactonModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
-    <>
-      <Header />
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactonModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
