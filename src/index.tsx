86f7d2241a3ea233297cdf838be39670762f4e8b
Gustavo Talano <gustavotalano@hotmail.com>
2021-10-15 13:55:13 -0300

    feat: adds mirage into index tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-10-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Moradia',
+          amount: 1000,
+          createdAt: new Date('2021-10-15 12:00:00'),
+        }
+      ],
+    });
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, response) => {
+      const data = JSON.parse(response.requestBody);
+
+      return schema.create('transaction', data);
+    })
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
