3b70f2ef768c6761af2dd56ab4ef07683c18c649
Gustavo Talano <gustavotalano@hotmail.com>
2021-10-15 13:52:54 -0300

    refactor: adjustments into summary component

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,13 +1,38 @@
+// import { useContext } from 'react';
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import totalImg from '../../assets/total.svg'
+// import { TransactionsContext } from '../../TransactionContext';
+
 import { Container } from "./styles";
 
 export function Summary() {
+  // const data = useContext(TransactionsContext);
+
     return (
         <Container>
             <div>
                 <header>
                     <p>Entradas</p>
-                    <img src="" alt="" />
+                    <img src={incomeImg} alt="Entradas" />
                 </header>
+                <strong>R$ 1.000,00</strong>
+            </div>
+
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={outcomeImg} alt="Saídas" />
+                </header>
+                <strong>- R$ 200,00</strong>
+            </div>
+
+            <div className="highlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="Total" />
+                </header>
+                <strong>R$ 800,00</strong>
             </div>
         </Container>
     )
